1f9277115c7a0732d697da795adf9be261b23812
Maria Ivanova <95995635+mary-ivanova@users.noreply.github.com>
2023-04-23 15:15:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const play = document.querySelector('.about__play');
-const video = document.querySelector('.about__video video');
-
-play.addEventListener('click', () => {
-  video.play();
-  video.setAttribute('controls', 'controls');
-  play.classList.add('about__play-hidden');
-});
